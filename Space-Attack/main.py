914d6f5953f15efc563a1b2b1cfdbb6ef4c3f6d2
Clare Snyder <csnyder.of.hanover@gmail.com>
2014-11-04 13:21:12 -0500

    Set up basic app

--- Space-Attack/main.py
+++ Space-Attack/main.py
@@ -3,3 +3,14 @@ Created on Nov 3, 2014
 
 @author: claresnyder
 '''
+import pygame
+from pygameapp import PygameApp
+
+class SpaceAttackApp(PygameApp):
+    def __init__(self, screensize = (400,400)):
+        super().__init__(screensize = screensize, title="Space Attack!")
+        pygame.key.set_repeat(100)
+        self.setbackgroundcolor((0,0,50))
+        
+myapp = SpaceAttackApp()
+myapp.run(100)
