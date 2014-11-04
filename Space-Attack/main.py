c621f22b0c8ecc967f78f18927ec246957903998
Ezekiel Elin <ezekielelin@me.com>
2014-11-04 13:46:50 -0500

    Changed spritegroup

--- Space-Attack/main.py
+++ Space-Attack/main.py
@@ -8,12 +8,11 @@ from pygameapp import PygameApp
 from actor import Actor
 
 class SpaceAttackApp(PygameApp):
-    actors = []
     def __init__(self, screensize = (400,400)):
         super().__init__(screensize = screensize, title="Space Attack!")
         pygame.key.set_repeat(100)
         self.setbackgroundcolor((0,0,50))
-        self.player = Player(5,5,15,15,self.actors)
+        self.player = Player(5,5,15,15,self.spritegroup)
         self.player.draw()
         self.player.color = (120,120,120)
         
